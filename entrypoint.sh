8f55e1db57c0813ffb862dc5881801be77e172a8
graviris-labs <andrew@graviris.com>
2025-04-28 22:04:14 -0700

    ch

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@ set -e
 source /opt/ros/humble/setup.bash
 source /ros2_ws/install/setup.bash
 # Run Livox driver
-ros2 run livox_ros2_driver livox_ros2_driver_node --ros-args -p auto_connect:=false -p user_config_path:=/ros2_ws/src/livox_ros2_driver/livox_ros2_driver/config/livox_lidar_config.json
+ros2 run livox_ros2_driver livox_ros2_driver_node --ros-args -p data_src:=0 -p user_config_path:=/ros2_ws/src/livox_ros2_driver/livox_ros2_driver/config/livox_lidar_config.json
+
